04b73e7ddfac28b9b018aab76fa7803c27525ea5
Alex Forencich <alex@alexforencich.com>
2025-02-11 22:12:57 -0800

    eth: Add 1G Ethernet MAC module with FIFOs and testbench
    Signed-off-by: Alex Forencich <alex@alexforencich.com>

---
+++ rtl/eth/taxi_eth_mac_1g_fifo.f
@@ -0,0 +1,3 @@
+taxi_eth_mac_1g_fifo.sv
+taxi_eth_mac_1g.f
+../axis/taxi_axis_async_fifo_adapter.f
---
+++ tb/eth/taxi_eth_mac_1g_fifo/test_taxi_eth_mac_1g_fifo.sv
@@ -0,0 +1,161 @@
+// SPDX-License-Identifier: CERN-OHL-S-2.0
+/*
+
+Copyright (c) 2025 FPGA Ninja, LLC
+
+Authors:
+- Alex Forencich
+
+*/
+
+`resetall
+`timescale 1ns / 1ps
+`default_nettype none
+
+/*
+ * 1G Ethernet MAC with TX and RX FIFOs testbench
+ */
+module test_taxi_eth_mac_1g_fifo #
+(
+    /* verilator lint_off WIDTHTRUNC */
+    parameter DATA_W = 8,
+    parameter AXIS_DATA_W = 8,
+    parameter logic PADDING_EN = 1'b1,
+    parameter MIN_FRAME_LEN = 64,
+    parameter TX_TAG_W = 16,
+    parameter TX_FIFO_DEPTH = 4096,
+    parameter TX_FIFO_RAM_PIPELINE = 1,
+    parameter logic TX_FRAME_FIFO = 1'b1,
+    parameter logic TX_DROP_OVERSIZE_FRAME = TX_FRAME_FIFO,
+    parameter logic TX_DROP_BAD_FRAME = TX_DROP_OVERSIZE_FRAME,
+    parameter logic TX_DROP_WHEN_FULL = 1'b0,
+    parameter TX_CPL_FIFO_DEPTH = 64,
+    parameter RX_FIFO_DEPTH = 4096,
+    parameter RX_FIFO_RAM_PIPELINE = 1,
+    parameter logic RX_FRAME_FIFO = 1'b1,
+    parameter logic RX_DROP_OVERSIZE_FRAME = RX_FRAME_FIFO,
+    parameter logic RX_DROP_BAD_FRAME = RX_DROP_OVERSIZE_FRAME,
+    parameter logic RX_DROP_WHEN_FULL = RX_DROP_OVERSIZE_FRAME
+    /* verilator lint_on WIDTHTRUNC */
+)
+();
+
+localparam TX_USER_W = 1;
+localparam RX_USER_W = 1;
+
+logic rx_clk;
+logic rx_rst;
+logic tx_clk;
+logic tx_rst;
+logic logic_clk;
+logic logic_rst;
+
+taxi_axis_if #(.DATA_W(AXIS_DATA_W), .USER_EN(1), .USER_W(TX_USER_W), .ID_EN(1), .ID_W(TX_TAG_W)) s_axis_tx();
+taxi_axis_if #(.DATA_W(96), .KEEP_W(1), .ID_EN(1), .ID_W(TX_TAG_W)) m_axis_tx_cpl();
+taxi_axis_if #(.DATA_W(AXIS_DATA_W), .USER_EN(1), .USER_W(RX_USER_W)) m_axis_rx();
+
+logic [DATA_W-1:0] gmii_rxd;
+logic gmii_rx_dv;
+logic gmii_rx_er;
+logic [DATA_W-1:0] gmii_txd;
+logic gmii_tx_en;
+logic gmii_tx_er;
+
+logic rx_clk_enable;
+logic tx_clk_enable;
+logic rx_mii_select;
+logic tx_mii_select;
+
+logic tx_error_underflow;
+logic tx_fifo_overflow;
+logic tx_fifo_bad_frame;
+logic tx_fifo_good_frame;
+logic rx_error_bad_frame;
+logic rx_error_bad_fcs;
+logic rx_fifo_overflow;
+logic rx_fifo_bad_frame;
+logic rx_fifo_good_frame;
+
+logic [7:0] cfg_ifg;
+logic cfg_tx_enable;
+logic cfg_rx_enable;
+
+taxi_eth_mac_1g_fifo #(
+    .DATA_W(DATA_W),
+    .PADDING_EN(PADDING_EN),
+    .MIN_FRAME_LEN(MIN_FRAME_LEN),
+    .TX_FIFO_DEPTH(TX_FIFO_DEPTH),
+    .TX_FIFO_RAM_PIPELINE(TX_FIFO_RAM_PIPELINE),
+    .TX_FRAME_FIFO(TX_FRAME_FIFO),
+    .TX_DROP_OVERSIZE_FRAME(TX_DROP_OVERSIZE_FRAME),
+    .TX_DROP_BAD_FRAME(TX_DROP_BAD_FRAME),
+    .TX_DROP_WHEN_FULL(TX_DROP_WHEN_FULL),
+    .TX_CPL_FIFO_DEPTH(TX_CPL_FIFO_DEPTH),
+    .RX_FIFO_DEPTH(RX_FIFO_DEPTH),
+    .RX_FIFO_RAM_PIPELINE(RX_FIFO_RAM_PIPELINE),
+    .RX_FRAME_FIFO(RX_FRAME_FIFO),
+    .RX_DROP_OVERSIZE_FRAME(RX_DROP_OVERSIZE_FRAME),
+    .RX_DROP_BAD_FRAME(RX_DROP_BAD_FRAME),
+    .RX_DROP_WHEN_FULL(RX_DROP_WHEN_FULL)
+)
+uut (
+    .rx_clk(rx_clk),
+    .rx_rst(rx_rst),
+    .tx_clk(tx_clk),
+    .tx_rst(tx_rst),
+    .logic_clk(logic_clk),
+    .logic_rst(logic_rst),
+
+    /*
+     * Transmit interface (AXI stream)
+     */
+    .s_axis_tx(s_axis_tx),
+    .m_axis_tx_cpl(m_axis_tx_cpl),
+
+    /*
+     * Receive interface (AXI stream)
+     */
+    .m_axis_rx(m_axis_rx),
+
+    /*
+     * GMII interface
+     */
+    .gmii_rxd(gmii_rxd),
+    .gmii_rx_dv(gmii_rx_dv),
+    .gmii_rx_er(gmii_rx_er),
+    .gmii_txd(gmii_txd),
+    .gmii_tx_en(gmii_tx_en),
+    .gmii_tx_er(gmii_tx_er),
+
+    /*
+     * Control
+     */
+    .rx_clk_enable(rx_clk_enable),
+    .tx_clk_enable(tx_clk_enable),
+    .rx_mii_select(rx_mii_select),
+    .tx_mii_select(tx_mii_select),
+
+    /*
+     * Status
+     */
+    .tx_error_underflow(tx_error_underflow),
+    .tx_fifo_overflow(tx_fifo_overflow),
+    .tx_fifo_bad_frame(tx_fifo_bad_frame),
+    .tx_fifo_good_frame(tx_fifo_good_frame),
+    .rx_error_bad_frame(rx_error_bad_frame),
+    .rx_error_bad_fcs(rx_error_bad_fcs),
+    .rx_fifo_overflow(rx_fifo_overflow),
+    .rx_fifo_bad_frame(rx_fifo_bad_frame),
+    .rx_fifo_good_frame(rx_fifo_good_frame),
+
+    /*
+     * Configuration
+     */
+    .cfg_ifg(cfg_ifg),
+    .cfg_tx_enable(cfg_tx_enable),
+    .cfg_rx_enable(cfg_rx_enable)
+);
+
+endmodule
+
+`resetall
